c71fead3fa2c8547fe0cca329710adcf735d7109
AndreyGorbatyuk <ondrey2901@gmail.com>
2022-07-22 19:08:36 +0300

    коммит для загрузки лендинга на Github

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+	$('.header__slider').slick({
+		dots:true,
+		arrows:false,
+		fade:true,
+		autoplay:2000,
+		
+	});
+	$('.carousel__slider').slick({
+		dots:true,
+		arrows:false,
+		fade:true,
+		autoplay:2000,
+	})
+});
